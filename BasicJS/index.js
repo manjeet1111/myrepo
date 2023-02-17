c7460dd55fecae26ae171abb3b5050cdfd1474aa
manjeet1111 <manjeetkumar2779@gmail.com>
2023-02-17 22:37:15 +0530

    js class 2

--- BasicJS/index.js
+++ BasicJS/index.js
@@ -1,22 +1,29 @@
 
-// Factory Function
-function createObj(len,bre){
-    return rectangle={
-        length:len,
-        bradth:bre,
-        draw:function(){
-            console.log('Drawing Rectangle')
-        }
-    }    
-}
- let obj2=createObj(5,6);
+// // Factory Function
+// function createObj(len,bre){
+//     return rectangle={
+//         length:len,
+//         bradth:bre,
+//         draw:function(){
+//             console.log('Drawing Rectangle')
+//         }
+//     }    
+// }
+//  let obj2=createObj(5,6);
 
- // Constructor Function
- function Square(val){
-    this.side=val,  
-    function show(){
-        console.log('Showing Square');
-    }
- }
- let newObj=new Square(2);
-
+//  // Constructor Function
+//  function Square(val){
+//     this.side=val,  
+//     function show(){
+//         console.log('Showing Square');
+//     }
+//  }
+//  let newObj=new Square(2);
+let src={
+    value:4,
+    value1:3
+};
+let dest={...src}
+console.log(dest)
+src.value++;
+console.log(src)
