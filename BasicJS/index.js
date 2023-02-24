c8b2408975f730b05a6c0b8cd4f78ec20e893622
manjeet1111 <manjeetkumar2779@gmail.com>
2023-02-24 22:18:58 +0530

    js learning

--- BasicJS/index.js
+++ BasicJS/index.js
@@ -19,11 +19,18 @@
 //     }
 //  }
 //  let newObj=new Square(2);
-let src={
-    value:4,
-    value1:3
-};
-let dest={...src}
-console.log(dest)
-src.value++;
-console.log(src)
+// let src={
+//     value:4,
+//     value1:3
+// };
+// let dest={...src}
+// console.log(dest)
+// src.value++;
+// console.log(src)
+let atag=document.querySelectorAll("h2");
+//  console.log(atag[2]);
+// let event1=atag[2];
+atag[2].addEventListener("click",hello);
+function hello(){
+    console.log("hello");
+}
